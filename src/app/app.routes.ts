6884cccac5c31a63df3abc5f6f585e340c431431
harichandanavitasoft <harichandanavita@gmail.com>
2023-12-20 22:13:10 +0530

    adminhomepage created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
 import { RouterModule } from '@angular/router';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
 
 export const routes: Routes = [
     {path:'',component:HomepageComponent},
     {path:"homepage",component:HomepageComponent},
-    {path :"adminlogin",component:AdminloginComponent}
+    {path :"adminlogin",component:AdminloginComponent},
+    {path:"adminhome",component:AdminhomeComponent}
 ];
