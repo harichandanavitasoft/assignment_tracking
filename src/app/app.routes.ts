9a0a584588598ab1cade69df7f9cbc5f2fde0b99
harichandanavitasoft <harichandanavita@gmail.com>
2023-12-20 21:41:56 +0530

    adminlogin page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-    {path:'',redirectTo :'homepage',pathMatch:"full"},
-    {path:"homepage",component:HomepageComponent}
+    {path:'',component:HomepageComponent},
+    {path:"homepage",component:HomepageComponent},
+    {path :"adminlogin",component:AdminloginComponent}
 ];
